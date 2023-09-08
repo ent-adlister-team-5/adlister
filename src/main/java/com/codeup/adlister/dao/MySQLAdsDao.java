2888ebcd3a4dc54f5f0cb7d01ae4bfaa1362526c
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-09-08 11:52:47 -0500

    refactored extractAd method in MySQLAdsDao

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -122,7 +122,11 @@ public class MySQLAdsDao implements Ads {
                 rs.getLong("id"),
                 rs.getLong("user_id"),
                 rs.getString("title"),
-                rs.getString("description")
+                rs.getString("description"),
+                rs.getString("date"),
+                rs.getString("time"),
+                rs.getString("location"),
+                rs.getBoolean("cancelled")
         );
     }
 
