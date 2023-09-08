e549325b450db1a9e615ce25866836d5e78d9698
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-09-08 14:25:34 -0500

    saved changes on idDetails and searchdetails

--- src/main/java/com/codeup/adlister/controllers/SearchDetailsServlet.java
+++ src/main/java/com/codeup/adlister/controllers/SearchDetailsServlet.java
@@ -9,29 +9,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "controllers.DetailsViewServlet", urlPatterns = "/details")
-public class DetailsViewServlet extends HttpServlet {
+@WebServlet(name = "controllers.SearchDetailsServlet", urlPatterns = "/search")
+public class SearchDetailsServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        String searchAd = req.getParameter("search");
+        List<Ad> ads = DaoFactory.getAdsDao().findAdbyTitle(searchAd);
+
+        if(ads == null) {
+            req.setAttribute("message", "No matching ads found");
+            req.getRequestDispatcher("/WEB-INF/testPackage/testSearchView.jsp").forward(req, resp);
+            return;
+        }
+//        if(ad.getTitle().equalsIgnoreCase(searchAd) || searchAd.toLowerCase().contains(ad.getTitle().toLowerCase())) {
+//            req.setAttribute("ad", ad);
+//        }
+        req.setAttribute("ads", ads);
+        req.getRequestDispatcher("/WEB-INF/testPackage/testSearchView.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String searchAd = req.getParameter("search");
-        Ad ad = DaoFactory.getAdsDao().findAdbyTitle(searchAd);
-
-        if(ad == null) {
-            req.setAttribute("message", "No matching ad found");
-            req.getRequestDispatcher("/WEB-INF/testPackage/testSearchView.jsp").forward(req, resp);
-            return;
-        }
-        if(ad.getTitle().equalsIgnoreCase(searchAd) || searchAd.toLowerCase().contains(ad.getTitle().toLowerCase())) {
-            req.setAttribute("ad", ad);
-        }
-        req.getRequestDispatcher("/WEB-INF/testPackage/testSearchView.jsp").forward(req, resp);
 
     }
 
