edb8ac2c3e5b9988cb7c8e51cb34e3b58b5111ef
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-09-09 12:54:03 -0500

    final update with full functionality to update all columns in ads table, INCLUDING boolean cancelled

--- src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
@@ -25,7 +25,7 @@ public class EditAdServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         /** waiting on MySQLAdsDao method, will receive response and set things */
         Long adId = Long.parseLong(req.getParameter("id"));
-        boolean cancelled = req.getParameter("cancel") != null;
+        boolean cancelled = req.getParameter("cancel") != null; // Check if the cancel parameter is empty, full expression evaluated
         System.out.println("Cancel parameter value: " + req.getParameter("cancel"));
 
         DaoFactory.getAdsDao().editAd(
