3b1a9623f60d81bb5db1e568ff4702b1214a1c6e
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-09-11 12:07:32 -0500

    created EditUserServlet

---
+++ src/main/java/com/codeup/adlister/controllers/EditUserServlet.java
@@ -0,0 +1,8 @@
+package com.codeup.adlister.controllers;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "controllers.EditUserServlet", urlPatterns = "/edit-profile")
+public class EditUserServlet extends HttpServlet {
+}
