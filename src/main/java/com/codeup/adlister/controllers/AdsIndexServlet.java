8966cd2a7a177d2106cdf9d6d81a9109e210d541
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-09-11 22:10:25 -0500

    all ads button on navbar

--- src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
@@ -3,6 +3,7 @@ package com.codeup.adlister.controllers;
 import com.codeup.adlister.dao.DaoFactory;
 
 import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
 
 
 import javax.servlet.ServletException;
@@ -16,16 +17,17 @@ import java.util.List;
 @WebServlet(name = "controllers.AdsIndexServlet", urlPatterns = "/ads")
 public class AdsIndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User loggedInUser = (User) request.getSession().getAttribute("user");
+        request.setAttribute("user", loggedInUser);
 
         String searchQuery = request.getParameter("search");
+
         if (searchQuery != null && !searchQuery.isEmpty()) {
             List<Ad> searchResults = DaoFactory.getAdsDao().findAdbyTitle(searchQuery);
             request.setAttribute("ads", searchResults);
         } else {
             request.setAttribute("ads", DaoFactory.getAdsDao().all());
         }
-//        request.setAttribute("ads", DaoFactory.getAdsDao().all());
-
         request.getRequestDispatcher("/WEB-INF/ads/ads.jsp").forward(request, response);
     }
 }
