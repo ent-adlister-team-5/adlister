60354bcc9f1e1631249d302aa81a4e0d6f037ffa
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-09-12 16:20:20 -0500

    setting up file-stack file picker

--- src/main/webapp/js/file-picker-utils.js
+++ src/main/webapp/js/file-picker-utils.js
@@ -6,6 +6,15 @@ export class FilePickerAPI {
         const client = await filestack.init(FILE_STACK_API);
         const options = {
             fromSources: ["local_file_system", "url"],
+            transformations: {
+                crop: { aspectRatio: 1 / 1, force: true },
+            },
+            imageMin: [200, 200],
+            imageMax: [200, 200],
+            onFileUploadFinished: file => {
+                console.log(file);
+
+            }
         };
         client.picker(options).open();
     }
--- src/main/webapp/js/file-picker.js
+++ src/main/webapp/js/file-picker.js
@@ -1,5 +1,12 @@
 (() => {
 
+    const click = document.querySelector("#click-here");
+
+    click.addEventListener("click", async() =>  {
+        const filePickerVar = await FilePickerAPI.getFilePicker();
+        console.log(filePickerVar);
+
+    })
 
 
 }) ();
